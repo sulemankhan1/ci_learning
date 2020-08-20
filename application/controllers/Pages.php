f6411ec40b10d68309ea3fdc20a19467cc465f44
sulemank11 <=>
2020-08-20 20:58:18 +0500

    class 2: query builder class

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -5,16 +5,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Pages extends CI_Controller {
 
   public function index($name="") {
-    if($name == "") {
-      redirect("/");
-    }
 
-    $this->db->select("*");
-    $this->db->from('pages');
-    $this->db->where('name', $name);
-    $page = $this->db->get()->row_array();
+    // $result = $this->db->get_where('salaries', array('salary' => 4000, 'id' => 5))->result();
 
-    $this->load->view('pages/page', array('page' => $page));
+    $this->db->select_max("salaries.salary");
+    $this->db->select("salaries.user_id");
+    $this->db->from("salaries");
+    $record = $this->db->get()->result();
+
+    echo $this->db->last_query();
+
+  }
+
+  public function insert() {
+    $data = array(
+      array(
+        'user_id' => 1,
+        'salary' => 5000
+      ),
+      array(
+        'user_id' => 1,
+        'salary' => 6000
+      ),
+      array(
+        'user_id' => 1,
+        'salary' => 3000
+      ),
+      array(
+        'user_id' => 2,
+        'salary' => 3500
+      ),
+      array(
+        'user_id' => 2,
+        'salary' => 4000
+      ),
+    );
+    $this->db->insert_batch('salaries', $data);
   }
 
 }
