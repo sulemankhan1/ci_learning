bc124456cc1f69165dac298dc08bf20a54975be6
sulemank11 <=>
2020-08-23 22:23:29 +0500

    update / delete

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -4,43 +4,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pages extends CI_Controller {
 
-  public function index($name="") {
+  public function index() {
 
-    // $result = $this->db->get_where('salaries', array('salary' => 4000, 'id' => 5))->result();
 
-    $this->db->select_max("salaries.salary");
-    $this->db->select("salaries.user_id");
-    $this->db->from("salaries");
-    $record = $this->db->get()->result();
-
-    echo $this->db->last_query();
 
   }
 
-  public function insert() {
-    $data = array(
-      array(
-        'user_id' => 1,
-        'salary' => 5000
-      ),
-      array(
-        'user_id' => 1,
-        'salary' => 6000
-      ),
-      array(
-        'user_id' => 1,
-        'salary' => 3000
-      ),
-      array(
-        'user_id' => 2,
-        'salary' => 3500
-      ),
-      array(
-        'user_id' => 2,
-        'salary' => 4000
-      ),
-    );
-    $this->db->insert_batch('salaries', $data);
+  public function update($where_column, $where_value, $update_column, $update_value, $tbl_name) {
+    $this->db->set($update_column, $update_value);
+    $this->db->where_in($where_column, $where_value);
+    $this->db->update($tbl_name);
+  }
+
+  public function joins() {
+    $this->db->select("users.name, SUM(salaries.salary) as salary");
+    $this->db->from("salaries");
+    $this->db->join('users', 'users.id = salaries.user_id AND users.is_deleted = 0');
+    $this->db->group_by("salaries.user_id");
+    $record = $this->db->get()->result();
+
+     echo "<pre>";
+     print_r($record);
+     die();
+  }
+
+  public function delete() {
+    // delete
+    // delete from 'pages' where id = 1;
+    $this->db->where('id', 3);
+    $this->db->delete('salaries');
   }
 
 }
